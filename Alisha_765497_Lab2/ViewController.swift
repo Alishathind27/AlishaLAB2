241bfce98bc369a1152cf23172f98e99e2320ba9
Alisha <alisha27011997@gmail.com>
2020-01-19 15:37:09 -0500

    Initial Commit

---
+++ Alisha_765497_Lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alisha_765497_Lab2
+//
+//  Created by Alisha Thind on 2020-01-19.
+//  Copyright © 2020 Alisha Thind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
